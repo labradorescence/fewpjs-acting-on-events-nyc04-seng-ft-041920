c1eb4ea7e1d62d05226d37adc87eea69646f0271
Greem <52639837+labradorescence@users.noreply.github.com>
2020-06-27 17:48:12 -0400

    move dodger right update index.js
    previous moveDodgerRight() will let the dodger disappear into the right side of the box. So this code won't let that happen.

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '');
-  var left = parseInt(leftNumbers, 10)
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`
+  const rightStr = dodger.style.left.replace("px", "")
+  const right = parseInt(rightStr, 10)
+  if(right < 360){
+    dodger.style.left = `${right + 1}px`
   }
-}
 
 document.addEventListener('keydown', function(e) {
   if (e.key === "ArrowLeft") {
